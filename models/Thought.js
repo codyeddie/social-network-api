0f3939f7b7f82fedfbc6856c68a1eec36c6e6ec8
Cody Eddie <buffbuck0427@gmail.com>
2022-08-22 19:44:04 -0600

    create thought model

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model, Types } = require('mongoose');
+
+// create a schema for a thought
+const ThoughtSchema = new Schema(
+    {   
+        username: {
+            type: String,
+        },
+        thoughtText: {
+            type: String,
+        },
+        reactions: [ReactionSchema],
+
+    }
+);
+
+// create a schema for a reaction
+const ReactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+        type: String
+      },
+      username: {
+        type: String,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    }
+  );
+
+// create the thought model 
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
+
